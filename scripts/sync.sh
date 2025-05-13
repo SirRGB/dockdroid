ac3a72e72da1e2446ea4a68efdfb1733189ec2b8
SirRGB <sirrgb@proton.me>
2025-05-13 23:11:27 +0200

    sync: accept errors differently
    * should help with tg messages about falsely failing syncs

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -21,7 +21,9 @@ _sync() {
   threads=$(nproc)
   repo forall -c "rm .git/*.lock" || true
   repo sync --current-branch --force-remove-dirty --force-sync --no-tags --no-clone-bundle --retry-fetches=25 --jobs="${threads}" --jobs-network=$((threads < 16 ? threads : 16)) | tee -a "${LOGS_DIR}"/"${BUILD_DATE}"/sync.txt
-  repo forall -c "git lfs pull" || true
+  set +e
+  repo forall -c "git lfs pull"
+  set -e
   unset ROM_DIR ROM_MANIFEST LOCAL_MANIFEST
 }
 
