5279535e9c3a39e5eeb49e9a667ff2fb8e9526f2
SirRGB <sirrgb@proton.me>
2025-05-21 16:18:25 +0200

    sync: create local manifests dir manually
    * otherwise find fails because it can not find the folder

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -5,13 +5,13 @@ source "${SCRIPT_DIR}"/print.sh
 
 # Pull manifest, local manifest and sync
 _sync() {
-  if [[ ! -d "${ROM_DIR}" ]]; then
-    mkdir -p "${ROM_DIR}"
+  if [[ ! -d "${ROM_DIR}"/.repo/local_manifests ]]; then
+    mkdir -p "${ROM_DIR}"/.repo/local_manifests
   fi
   cd "${ROM_DIR}" || exit
   repo init -u "${ROM_MANIFEST}" -b "${ROM_BRANCH}" --depth=1 -g default,-darwin --git-lfs --no-clone-bundle 2>&1 | tee -a "${LOGS_DIR}"/"${BUILD_DATE}"/sync.txt
   find "${ROM_DIR}"/.repo/local_manifests/ -type f -exec rm {} \;
-  curl "${LOCAL_MANIFEST}" --create-dirs --output .repo/local_manifests/manifest.xml
+  curl "${LOCAL_MANIFEST}" --output .repo/local_manifests/manifest.xml
   local threads
   threads=$(nproc)
   repo forall -c "rm .git/*.lock" || true
