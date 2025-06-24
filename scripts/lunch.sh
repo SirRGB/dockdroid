5cc8e5e62b34c49ff9bc05d475df4ebf246e56c3
sirrgb <sirrgb@proton.me>
2025-06-24 14:40:17 +0200

    sign: update apex key list
    * use arrays to avoid duplication
    * sync with https://github.com/LineageOS/scripts/commit/7008afaed40dea154095d688a13bc84aaefbd96d

--- scripts/lunch.sh
+++ scripts/lunch.sh
@@ -23,7 +23,80 @@ _keysgen() {
     fi
   done
 
-  for apex in com.android.adbd com.android.adservices com.android.adservices.api com.android.appsearch com.android.appsearch.apk com.android.art com.android.bluetooth com.android.btservices com.android.cellbroadcast com.android.compos com.android.configinfrastructure com.android.connectivity.resources com.android.conscrypt com.android.devicelock com.android.extservices com.android.graphics.pdf com.android.hardware.authsecret com.android.hardware.biometrics.face.virtual com.android.hardware.biometrics.fingerprint.virtual com.android.hardware.boot com.android.hardware.cas com.android.hardware.neuralnetworks com.android.hardware.rebootescrow com.android.hardware.wifi com.android.healthfitness com.android.hotspot2.osulogin com.android.i18n com.android.ipsec com.android.media com.android.media.swcodec com.android.mediaprovider com.android.nearby.halfsheet com.android.networkstack.tethering com.android.neuralnetworks com.android.nfcservices com.android.ondevicepersonalization com.android.os.statsd com.android.permission com.android.profiling com.android.resolv com.android.rkpd com.android.runtime com.android.safetycenter.resources com.android.scheduling com.android.sdkext com.android.support.apexer com.android.telephony com.android.telephonymodules com.android.tethering com.android.tzdata com.android.uwb com.android.uwb.resources com.android.virt com.android.vndk.current com.android.vndk.current.on_vendor com.android.wifi com.android.wifi.dialog com.android.wifi.resources com.google.pixel.camera.hal com.google.pixel.vibrator.hal com.qorvo.uwb; do
+  export APEX_KEYS
+  APEX_KEYS=(
+    com.android.adbd
+    com.android.adservices
+    com.android.adservices.api
+    com.android.appsearch
+    com.android.appsearch.apk
+    com.android.art
+    com.android.bluetooth
+    com.android.bt
+    com.android.btservices
+    com.android.cellbroadcast
+    com.android.compos
+    com.android.configinfrastructure
+    com.android.connectivity.resources
+    com.android.conscrypt
+    com.android.crashrecovery
+    com.android.devicelock
+    com.android.extservices
+    com.android.hardware.authsecret
+    com.android.hardware.biometrics.face.virtual
+    com.android.hardware.biometrics.fingerprint.virtual
+    com.android.hardware.boot
+    com.android.hardware.cas
+    com.android.hardware.contexthub
+    com.android.hardware.dumpstate
+    com.android.hardware.gatekeeper.nonsecure
+    com.android.hardware.neuralnetworks
+    com.android.hardware.power
+    com.android.hardware.rebootescrow
+    com.android.hardware.thermal
+    com.android.hardware.threadnetwork
+    com.android.hardware.uwb
+    com.android.hardware.vibrator
+    com.android.hardware.wifi
+    com.android.healthfitness
+    com.android.hotspot2.osulogin
+    com.android.i18n
+    com.android.ipsec
+    com.android.media
+    com.android.media.swcodec
+    com.android.mediaprovider
+    com.android.nearby.halfsheet
+    com.android.networkstack.tethering
+    com.android.neuralnetworks
+    com.android.nfcservices
+    com.android.ondevicepersonalization
+    com.android.os.statsd
+    com.android.permission
+    com.android.profiling
+    com.android.resolv
+    com.android.rkpd
+    com.android.runtime
+    com.android.safetycenter.resources
+    com.android.scheduling
+    com.android.sdkext
+    com.android.support.apexer
+    com.android.telephony
+    com.android.telephonymodules
+    com.android.tethering
+    com.android.tzdata
+    com.android.uprobestats
+    com.android.uwb
+    com.android.uwb.resources
+    com.android.virt
+    com.android.vndk.current
+    com.android.wifi
+    com.android.wifi.dialog
+    com.android.wifi.resources
+    com.google.pixel.vibrator.hal
+    com.qorvo.uwb
+  )
+
+  for apex in "${APEX_KEYS[@]}"; do
     if [[ ! -f "${KEYS_DIR}"/"${apex}".x509.pem ]] || [[ ! -f "${KEYS_DIR}"/"${apex}".pem ]] ; then
       subject="${KEYS_SUBJECT//CN=Android/CN=\$\{apex\}}"
       make_key "${KEYS_DIR}"/"${apex}" "${subject}"
--- scripts/sign.sh
+++ scripts/sign.sh
@@ -42,6 +42,10 @@ _sign_old() {
 
 # New signing process (APEX), A12/up
 _sign_new() {
+  for apex_key in "${APEX_KEYS[@]}"; do
+    APEX_ARGS+=("--extra_apks" "${apex_key}.apex=${KEYS_DIR}/${apex_key}" "--extra_apex_payload_key" "${apex_key}.apex=${KEYS_DIR}/${apex_key}.pem")
+  done
+
   set +eu
   sign_target_files_apks -o -d "${KEYS_DIR}" \
       --extra_apks AdServicesApk.apk="${KEYS_DIR}"/releasekey \
@@ -55,119 +59,11 @@ _sign_new() {
       --extra_apks ServiceUwbResources.apk="${KEYS_DIR}"/releasekey \
       --extra_apks ServiceWifiResources.apk="${KEYS_DIR}"/releasekey \
       --extra_apks WifiDialog.apk="${KEYS_DIR}"/releasekey \
-      --extra_apks com.android.adbd.apex="${KEYS_DIR}"/com.android.adbd \
-      --extra_apks com.android.adservices.apex="${KEYS_DIR}"/com.android.adservices \
-      --extra_apks com.android.adservices.api.apex="${KEYS_DIR}"/com.android.adservices.api \
-      --extra_apks com.android.appsearch.apex="${KEYS_DIR}"/com.android.appsearch \
-      --extra_apks com.android.art.apex="${KEYS_DIR}"/com.android.art \
-      --extra_apks com.android.bluetooth.apex="${KEYS_DIR}"/com.android.bluetooth \
-      --extra_apks com.android.btservices.apex="${KEYS_DIR}"/com.android.btservices \
-      --extra_apks com.android.cellbroadcast.apex="${KEYS_DIR}"/com.android.cellbroadcast \
-      --extra_apks com.android.compos.apex="${KEYS_DIR}"/com.android.compos \
-      --extra_apks com.android.configinfrastructure.apex="${KEYS_DIR}"/com.android.configinfrastructure \
-      --extra_apks com.android.connectivity.resources.apex="${KEYS_DIR}"/com.android.connectivity.resources \
-      --extra_apks com.android.conscrypt.apex="${KEYS_DIR}"/com.android.conscrypt \
-      --extra_apks com.android.devicelock.apex="${KEYS_DIR}"/com.android.devicelock \
-      --extra_apks com.android.extservices.apex="${KEYS_DIR}"/com.android.extservices \
-      --extra_apks com.android.graphics.pdf.apex="${KEYS_DIR}"/com.android.graphics.pdf \
-      --extra_apks com.android.hardware.biometrics.face.virtual.apex="${KEYS_DIR}"/com.android.hardware.biometrics.face.virtual \
-      --extra_apks com.android.hardware.biometrics.fingerprint.virtual.apex="${KEYS_DIR}"/com.android.hardware.biometrics.fingerprint.virtual \
-      --extra_apks com.android.hardware.boot.apex="${KEYS_DIR}"/com.android.hardware.boot \
-      --extra_apks com.android.hardware.cas.apex="${KEYS_DIR}"/com.android.hardware.cas \
-      --extra_apks com.android.hardware.wifi.apex="${KEYS_DIR}"/com.android.hardware.wifi \
-      --extra_apks com.android.healthfitness.apex="${KEYS_DIR}"/com.android.healthfitness \
-      --extra_apks com.android.hotspot2.osulogin.apex="${KEYS_DIR}"/com.android.hotspot2.osulogin \
-      --extra_apks com.android.i18n.apex="${KEYS_DIR}"/com.android.i18n \
-      --extra_apks com.android.ipsec.apex="${KEYS_DIR}"/com.android.ipsec \
-      --extra_apks com.android.media.apex="${KEYS_DIR}"/com.android.media \
-      --extra_apks com.android.media.swcodec.apex="${KEYS_DIR}"/com.android.media.swcodec \
-      --extra_apks com.android.mediaprovider.apex="${KEYS_DIR}"/com.android.mediaprovider \
-      --extra_apks com.android.nearby.halfsheet.apex="${KEYS_DIR}"/com.android.nearby.halfsheet \
-      --extra_apks com.android.networkstack.tethering.apex="${KEYS_DIR}"/com.android.networkstack.tethering \
-      --extra_apks com.android.neuralnetworks.apex="${KEYS_DIR}"/com.android.neuralnetworks \
-      --extra_apks com.android.ondevicepersonalization.apex="${KEYS_DIR}"/com.android.ondevicepersonalization \
-      --extra_apks com.android.os.statsd.apex="${KEYS_DIR}"/com.android.os.statsd \
-      --extra_apks com.android.permission.apex="${KEYS_DIR}"/com.android.permission \
-      --extra_apks com.android.resolv.apex="${KEYS_DIR}"/com.android.resolv \
-      --extra_apks com.android.rkpd.apex="${KEYS_DIR}"/com.android.rkpd \
-      --extra_apks com.android.runtime.apex="${KEYS_DIR}"/com.android.runtime \
-      --extra_apks com.android.safetycenter.resources.apex="${KEYS_DIR}"/com.android.safetycenter.resources \
-      --extra_apks com.android.scheduling.apex="${KEYS_DIR}"/com.android.scheduling \
-      --extra_apks com.android.sdkext.apex="${KEYS_DIR}"/com.android.sdkext \
-      --extra_apks com.android.support.apexer.apex="${KEYS_DIR}"/com.android.support.apexer \
-      --extra_apks com.android.telephony.apex="${KEYS_DIR}"/com.android.telephony \
-      --extra_apks com.android.telephonymodules.apex="${KEYS_DIR}"/com.android.telephonymodules \
-      --extra_apks com.android.tethering.apex="${KEYS_DIR}"/com.android.tethering \
-      --extra_apks com.android.tzdata.apex="${KEYS_DIR}"/com.android.tzdata \
-      --extra_apks com.android.uwb.apex="${KEYS_DIR}"/com.android.uwb \
-      --extra_apks com.android.uwb.resources.apex="${KEYS_DIR}"/com.android.uwb.resources \
-      --extra_apks com.android.virt.apex="${KEYS_DIR}"/com.android.virt \
-      --extra_apks com.android.vndk.current.apex="${KEYS_DIR}"/com.android.vndk.current \
-      --extra_apks com.android.vndk.current.on_vendor.apex="${KEYS_DIR}"/com.android.vndk.current.on_vendor \
-      --extra_apks com.android.wifi.apex="${KEYS_DIR}"/com.android.wifi \
-      --extra_apks com.android.wifi.dialog.apex="${KEYS_DIR}"/com.android.wifi.dialog \
-      --extra_apks com.android.wifi.resources.apex="${KEYS_DIR}"/com.android.wifi.resources \
-      --extra_apks com.google.pixel.camera.hal.apex="${KEYS_DIR}"/com.google.pixel.camera.hal \
-      --extra_apks com.google.pixel.vibrator.hal.apex="${KEYS_DIR}"/com.google.pixel.vibrator.hal \
-      --extra_apks com.qorvo.uwb.apex="${KEYS_DIR}"/com.qorvo.uwb \
-      --extra_apex_payload_key com.android.adbd.apex="${KEYS_DIR}"/com.android.adbd.pem \
-      --extra_apex_payload_key com.android.adservices.apex="${KEYS_DIR}"/com.android.adservices.pem \
-      --extra_apex_payload_key com.android.adservices.api.apex="${KEYS_DIR}"/com.android.adservices.api.pem \
-      --extra_apex_payload_key com.android.appsearch.apex="${KEYS_DIR}"/com.android.appsearch.pem \
-      --extra_apex_payload_key com.android.art.apex="${KEYS_DIR}"/com.android.art.pem \
-      --extra_apex_payload_key com.android.bluetooth.apex="${KEYS_DIR}"/com.android.bluetooth.pem \
-      --extra_apex_payload_key com.android.btservices.apex="${KEYS_DIR}"/com.android.btservices.pem \
-      --extra_apex_payload_key com.android.cellbroadcast.apex="${KEYS_DIR}"/com.android.cellbroadcast.pem \
-      --extra_apex_payload_key com.android.compos.apex="${KEYS_DIR}"/com.android.compos.pem \
-      --extra_apex_payload_key com.android.configinfrastructure.apex="${KEYS_DIR}"/com.android.configinfrastructure.pem \
-      --extra_apex_payload_key com.android.connectivity.resources.apex="${KEYS_DIR}"/com.android.connectivity.resources.pem \
-      --extra_apex_payload_key com.android.conscrypt.apex="${KEYS_DIR}"/com.android.conscrypt.pem \
-      --extra_apex_payload_key com.android.devicelock.apex="${KEYS_DIR}"/com.android.devicelock.pem \
-      --extra_apex_payload_key com.android.extservices.apex="${KEYS_DIR}"/com.android.extservices.pem \
-      --extra_apex_payload_key com.android.graphics.pdf.apex="${KEYS_DIR}"/com.android.graphics.pdf.pem \
-      --extra_apex_payload_key com.android.hardware.biometrics.face.virtual.apex="${KEYS_DIR}"/com.android.hardware.biometrics.face.virtual.pem \
-      --extra_apex_payload_key com.android.hardware.biometrics.fingerprint.virtual.apex="${KEYS_DIR}"/com.android.hardware.biometrics.fingerprint.virtual.pem \
-      --extra_apex_payload_key com.android.hardware.boot.apex="${KEYS_DIR}"/com.android.hardware.boot.pem \
-      --extra_apex_payload_key com.android.hardware.cas.apex="${KEYS_DIR}"/com.android.hardware.cas.pem \
-      --extra_apex_payload_key com.android.hardware.wifi.apex="${KEYS_DIR}"/com.android.hardware.wifi.pem \
-      --extra_apex_payload_key com.android.healthfitness.apex="${KEYS_DIR}"/com.android.healthfitness.pem \
-      --extra_apex_payload_key com.android.hotspot2.osulogin.apex="${KEYS_DIR}"/com.android.hotspot2.osulogin.pem \
-      --extra_apex_payload_key com.android.i18n.apex="${KEYS_DIR}"/com.android.i18n.pem \
-      --extra_apex_payload_key com.android.ipsec.apex="${KEYS_DIR}"/com.android.ipsec.pem \
-      --extra_apex_payload_key com.android.media.apex="${KEYS_DIR}"/com.android.media.pem \
-      --extra_apex_payload_key com.android.media.swcodec.apex="${KEYS_DIR}"/com.android.media.swcodec.pem \
-      --extra_apex_payload_key com.android.mediaprovider.apex="${KEYS_DIR}"/com.android.mediaprovider.pem \
-      --extra_apex_payload_key com.android.nearby.halfsheet.apex="${KEYS_DIR}"/com.android.nearby.halfsheet.pem \
-      --extra_apex_payload_key com.android.networkstack.tethering.apex="${KEYS_DIR}"/com.android.networkstack.tethering.pem \
-      --extra_apex_payload_key com.android.neuralnetworks.apex="${KEYS_DIR}"/com.android.neuralnetworks.pem \
-      --extra_apex_payload_key com.android.ondevicepersonalization.apex="${KEYS_DIR}"/com.android.ondevicepersonalization.pem \
-      --extra_apex_payload_key com.android.os.statsd.apex="${KEYS_DIR}"/com.android.os.statsd.pem \
-      --extra_apex_payload_key com.android.permission.apex="${KEYS_DIR}"/com.android.permission.pem \
-      --extra_apex_payload_key com.android.resolv.apex="${KEYS_DIR}"/com.android.resolv.pem \
-      --extra_apex_payload_key com.android.rkpd.apex="${KEYS_DIR}"/com.android.rkpd.pem \
-      --extra_apex_payload_key com.android.runtime.apex="${KEYS_DIR}"/com.android.runtime.pem \
-      --extra_apex_payload_key com.android.safetycenter.resources.apex="${KEYS_DIR}"/com.android.safetycenter.resources.pem \
-      --extra_apex_payload_key com.android.scheduling.apex="${KEYS_DIR}"/com.android.scheduling.pem \
-      --extra_apex_payload_key com.android.sdkext.apex="${KEYS_DIR}"/com.android.sdkext.pem \
-      --extra_apex_payload_key com.android.support.apexer.apex="${KEYS_DIR}"/com.android.support.apexer.pem \
-      --extra_apex_payload_key com.android.telephony.apex="${KEYS_DIR}"/com.android.telephony.pem \
-      --extra_apex_payload_key com.android.telephonymodules.apex="${KEYS_DIR}"/com.android.telephonymodules.pem \
-      --extra_apex_payload_key com.android.tethering.apex="${KEYS_DIR}"/com.android.tethering.pem \
-      --extra_apex_payload_key com.android.tzdata.apex="${KEYS_DIR}"/com.android.tzdata.pem \
-      --extra_apex_payload_key com.android.uwb.apex="${KEYS_DIR}"/com.android.uwb.pem \
-      --extra_apex_payload_key com.android.uwb.resources.apex="${KEYS_DIR}"/com.android.uwb.resources.pem \
-      --extra_apex_payload_key com.android.virt.apex="${KEYS_DIR}"/com.android.virt.pem \
-      --extra_apex_payload_key com.android.vndk.current.apex="${KEYS_DIR}"/com.android.vndk.current.pem \
-      --extra_apex_payload_key com.android.vndk.current.on_vendor.apex="${KEYS_DIR}"/com.android.vndk.current.on_vendor.pem \
-      --extra_apex_payload_key com.android.wifi.apex="${KEYS_DIR}"/com.android.wifi.pem \
-      --extra_apex_payload_key com.android.wifi.dialog.apex="${KEYS_DIR}"/com.android.wifi.dialog.pem \
-      --extra_apex_payload_key com.android.wifi.resources.apex="${KEYS_DIR}"/com.android.wifi.resources.pem \
-      --extra_apex_payload_key com.google.pixel.camera.hal.apex="${KEYS_DIR}"/com.google.pixel.camera.hal.pem \
-      --extra_apex_payload_key com.google.pixel.vibrator.hal.apex="${KEYS_DIR}"/com.google.pixel.vibrator.hal.pem \
-      --extra_apex_payload_key com.qorvo.uwb.apex="${KEYS_DIR}"/com.qorvo.uwb.pem \
+      "${APEX_ARGS[@]}" \
       "${OUT}"/obj/PACKAGING/target_files_intermediates/*-target_files*.zip \
       "${OUT}"/signed-target_files.zip 2>&1 | tee -a "${LOGS_DIR}"/"${BUILD_DATE}"/sign.txt
   set -eu
+  unset APEX_KEYS APEX_ARGS
 }
 
 _cleanup_fail() {
