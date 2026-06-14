63fe83979f1d6b1d4c9a80082bd88a310dd78e33
SirRGB <sirrgb@proton.me>
2026-06-14 18:24:55 +0200

    Revert "fix: adapt upload to gh api changes"
    This reverts commit ecf5156e533bc5f14e4d6ef7c11ec9dca03c680c.

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -31,9 +31,8 @@ _upload_gh() {
 
   # Create a release and get url
   upload_url=$(curl_cmd \
-    --url "https://api.github.com/octocat" \
     --request POST \
-    --header "Authorization: Bearer ${GITHUB_TOKEN}" \
+    --header "Authorization: token ${GITHUB_TOKEN}" \
     --header 'content-type: application/json' \
     https://api.github.com/repos/"${release_repo}"/releases \
     --data "{ \"tag_name\": \"${tag}\", \"body\": \"${desc}\" }" \
